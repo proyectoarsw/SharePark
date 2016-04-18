a7c92d0bba25439dbec853d643649129af21cae1
German Lopez <germaniuslp@hotmail.com>
2016-04-18 14:18:09 -0500

    #3 ingreso de entidades

---
+++ README.md
@@ -0,0 +1,2 @@
+# SharePark
+Este es un proyecto para la comunidad de personas que utlzan vehiculo o moto que desen compartir su parqueadero
---
+++ src/main/java/com/mycompany/sharepark/entidades/carro.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.sharepark.entidades;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 2096904
+ */
+public class carro extends Vehiculo {
+   
+}
+
